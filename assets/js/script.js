ac009ecb57550b2e289a53b1d8cb3b409d8d8e7b
Shane Wade <shanebramble96@hotmail.com>
2024-05-14 18:51:58 -0500

    included teams logos for 1 v 1  live matches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var todaysDate = moment().format("YYYY-MM-DD");
 
-
 function init() {
   var url =
-    "https://api-basketball.p.rapidapi.com/games?timezone=America%2FNew_York&season=2023-2024&league=12&date=" + todaysDate;
+    "https://api-basketball.p.rapidapi.com/games?timezone=America%2FNew_York&season=2023-2024&league=12&date=" +
+    todaysDate;
 
   fetch(url, {
     method: "GET",
@@ -27,21 +27,25 @@ function loadNowPlaying(nowData) {
 
     var cardHeader = document.createElement("div");
     // cardHeader.className = "card-header";
-    cardHeader.classList.add("card-header", "column")
+    cardHeader.classList.add("card-header", "column");
 
     var cardTitle = document.createElement("h3");
     // cardTitle.className = "card-title";
-    cardTitle.classList.add("card-title", "has-text-centered");
+    cardTitle.classList.add("card-header-title", "is-centered");
     cardTitle.textContent =
       nowData.response[i].teams.home.name +
       " vs " +
       nowData.response[i].teams.away.name;
-
+    // NBA team logos
     var cardImagecontainer = document.createElement("div");
     cardImagecontainer.className = "card-image";
-    var cardImage = document.createElement("img");
-    cardImage.setAttribute("src", "https://placehold.co/200x200");
-    cardImage.setAttribute("alt", "placeholder image");
+    var cardHomeImage = document.createElement("img");
+    cardHomeImage.setAttribute("src", nowData.response[i].teams.home.logo);
+    cardHomeImage.setAttribute("alt", "nba team image");
+
+    var cardAwayImage = document.createElement("img");
+    cardAwayImage.setAttribute("src", nowData.response[i].teams.away.logo);
+    cardAwayImage.setAttribute("alt", "nba team image");
 
     var cardContent = document.createElement("div");
     cardContent.className = "card-content";
@@ -49,7 +53,8 @@ function loadNowPlaying(nowData) {
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate minima ducimus autem laboriosam iusto illo dolorum,maiores ullam voluptatem nihil fugiat odio nobis neque liberotempore reprehenderit explicabo pariatur veniam.";
 
     cardHeader.appendChild(cardTitle);
-    cardImagecontainer.appendChild(cardImage);
+    cardImagecontainer.appendChild(cardHomeImage);
+    cardImagecontainer.appendChild(cardAwayImage);
     card.appendChild(cardHeader);
     card.appendChild(cardImagecontainer);
     card.appendChild(cardContent);
