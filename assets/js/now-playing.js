fd2d5ca9d8aaf7b864a8874d65f0c65bdb94e8d4
Shane Wade <shanebramble96@hotmail.com>
2024-06-24 18:51:08 -0500

    create the dynamic DOM elements for the now playing page

--- assets/js/now-playing.js
+++ assets/js/now-playing.js
@@ -15,8 +15,8 @@ var getNowPlaying = function () {
     },
   }).then(function (response) {
     response.json().then(function (data) {
-      loadNowPlayingGames(data);
-    //   console.log(data);
+      // loadNowPlayingGames(data);
+      console.log(data);
     });
   });
 };
@@ -29,9 +29,68 @@ function loadNowPlayingGames(gameData) {
   for (var i = 0; i < gameData.response.length; i++) {
     var currentGameContainer = document.createElement("div");
     currentGameContainer.classList.add("current-game level");
-    
-    var cur
-    
+
+    var currentGameItemContainer = document.createElement("div");
+    currentGameItemContainer.classList.add("level-item", "has-text-centered");
+
+    var gameCardMatchUpContainer = document.createElement("div");
+    gameCardMatchUpContainer.classList.add("gameCardMatchUpContainer");
+
+    var gameStatusTitle = document.createElement("p");
+    gameStatusTitle.classList.add("heading");
+    gameStatusTitle.textContent = gameData.response.status.long;
+
+    var gameContainer = document.createElement("div");
+    gameContainer.classList.add("game-container");
+
+    var awayContainer = document.createElement("article");
+    awayContainer.classList.add("away-container");
+
+    var awayImage = document.createElement("img");
+
+    var awayTeamContainer = document.createElement("div");
+    awayTeamContainer.classList.add("away-team-rank");
+
+    var awayTeamRank = document.createElement("p");
+    awayTeamRank.classList.add("away-rank");
+
+    var awayTeamName = document.createElement("p");
+    awayTeamName.classList.add("away-team");
+
+    var scoreContainer = document.createElement("div");
+    scoreContainer.classList.add("score-container");
+
+    var awayScore = document.createElement("div");
+    awayScore.classList.add("away-score");
+
+    var awayScoreItem = document.createElement("p");
+    awayScoreItem.classList.add("away-score-item");
+
+    var currentGameStatus = document.createElement("div");
+    currentGameStatus.classList.add("current-game-status");
+
+    var currentGameStatusItem = document.createElement("p");
+    currentGameStatusItem.classList.add("current-game-status-item");
+
+    var homeScore = document.createElement("div");
+    homeScore.classList.add("home-score");
+
+    var homeScoreItem = document.createElement("p");
+    homeScoreItem.classList.add("home-score-item");
+
+    var homeContainer = document.createElement("article");
+    homeContainer.classList.add("home-container");
+
+    var homeImage = document.createElement("img");
+
+    var homeTeamContainer = document.createElement("div");
+    homeTeamContainer.classList.add("home-team-rank");
+
+    var homeTeamRank = document.createElement("p");
+    homeTeamRank.classList.add("home-rank");
+
+    var homeTeamName = document.createElement("p");
+    homeTeamName.classList.add("home-team");
   }
 }
 document.querySelector("p.title").textContent = todaysDate;
