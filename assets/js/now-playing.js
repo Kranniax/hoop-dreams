a8ee504a8271fc6ce50219d04fd754dd54a4b190
Shane Wade <shanebramble96@hotmail.com>
2024-06-25 18:53:25 -0500

    create now playing games levels

--- assets/js/now-playing.js
+++ assets/js/now-playing.js
@@ -15,7 +15,7 @@ var getNowPlaying = function () {
     },
   }).then(function (response) {
     response.json().then(function (data) {
-      // loadNowPlayingGames(data);
+      loadNowPlayingGames(data);
       console.log(data);
     });
   });
@@ -28,7 +28,7 @@ function loadNowPlayingGames(gameData) {
 
   for (var i = 0; i < gameData.response.length; i++) {
     var currentGameContainer = document.createElement("div");
-    currentGameContainer.classList.add("current-game level");
+    currentGameContainer.classList.add("current-game", "level");
 
     var currentGameItemContainer = document.createElement("div");
     currentGameItemContainer.classList.add("level-item", "has-text-centered");
@@ -38,7 +38,7 @@ function loadNowPlayingGames(gameData) {
 
     var gameStatusTitle = document.createElement("p");
     gameStatusTitle.classList.add("heading");
-    gameStatusTitle.textContent = gameData.response.status.long;
+    gameStatusTitle.textContent = gameData.response[i].status.long;
 
     var gameContainer = document.createElement("div");
     gameContainer.classList.add("game-container");
@@ -47,6 +47,7 @@ function loadNowPlayingGames(gameData) {
     awayContainer.classList.add("away-container");
 
     var awayImage = document.createElement("img");
+    awayImage.setAttribute("src", gameData.response[i].teams.away.logo);
 
     var awayTeamContainer = document.createElement("div");
     awayTeamContainer.classList.add("away-team-rank");
@@ -56,6 +57,7 @@ function loadNowPlayingGames(gameData) {
 
     var awayTeamName = document.createElement("p");
     awayTeamName.classList.add("away-team");
+    awayTeamName.textContent = gameData.response[i].teams.away.name;
 
     var scoreContainer = document.createElement("div");
     scoreContainer.classList.add("score-container");
@@ -65,23 +67,27 @@ function loadNowPlayingGames(gameData) {
 
     var awayScoreItem = document.createElement("p");
     awayScoreItem.classList.add("away-score-item");
+    awayScoreItem.textContent = gameData.response[i].scores.away.total;
 
     var currentGameStatus = document.createElement("div");
     currentGameStatus.classList.add("current-game-status");
 
     var currentGameStatusItem = document.createElement("p");
     currentGameStatusItem.classList.add("current-game-status-item");
+    currentGameStatusItem.textContent = gameData.response[i].status.short;
 
     var homeScore = document.createElement("div");
     homeScore.classList.add("home-score");
 
     var homeScoreItem = document.createElement("p");
     homeScoreItem.classList.add("home-score-item");
+    homeScoreItem.textContent = gameData.response[i].scores.home.total;
 
     var homeContainer = document.createElement("article");
     homeContainer.classList.add("home-container");
 
     var homeImage = document.createElement("img");
+    homeImage.setAttribute("src", gameData.response[i].teams.home.logo);
 
     var homeTeamContainer = document.createElement("div");
     homeTeamContainer.classList.add("home-team-rank");
@@ -91,6 +97,39 @@ function loadNowPlayingGames(gameData) {
 
     var homeTeamName = document.createElement("p");
     homeTeamName.classList.add("home-team");
+    homeTeamName.textContent = gameData.response[i].teams.home.name;
+
+    awayTeamContainer.appendChild(awayTeamRank);
+    awayTeamContainer.appendChild(awayTeamName);
+    awayContainer.appendChild(awayImage);
+    awayContainer.appendChild(awayTeamContainer);
+
+    awayScore.appendChild(awayScoreItem);
+    currentGameStatus.appendChild(currentGameStatusItem);
+    homeScore.appendChild(homeScoreItem);
+
+    scoreContainer.appendChild(awayScore);
+    scoreContainer.appendChild(currentGameStatus);
+    scoreContainer.appendChild(homeScore);
+
+    homeTeamContainer.appendChild(homeTeamRank);
+    homeTeamContainer.appendChild(homeTeamName);
+    homeContainer.appendChild(homeImage);
+    homeContainer.appendChild(homeTeamContainer);
+
+    // Append all children to game container.
+    gameContainer.appendChild(awayContainer);
+    gameContainer.appendChild(scoreContainer);
+    gameContainer.appendChild(homeContainer);
+    // Append children elements to game match up container.
+    gameCardMatchUpContainer.appendChild(gameStatusTitle);
+    gameCardMatchUpContainer.appendChild(gameContainer);
+
+    currentGameItemContainer.appendChild(gameCardMatchUpContainer);
+    currentGameContainer.appendChild(currentGameItemContainer);
+    document
+      .querySelector(".now-playing-container")
+      .appendChild(currentGameContainer);
   }
 }
 document.querySelector("p.title").textContent = todaysDate;
