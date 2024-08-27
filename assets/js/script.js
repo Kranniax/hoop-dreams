4652cc2b704351fb8cd4786d4a1dca54bd5a7b51
Shane Wade <shanebramble96@hotmail.com>
2024-08-26 19:44:30 -0500

    created persistency to store recently searched NBA teams.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var todaysDate = "2024-05-19";
 var currentYear = moment().format("Y");
 var previousYear = moment().subtract(1, "y").format("Y");
 var teamInput = document.querySelector(".team-input");
+var searchHistory = [];
 
 function getNowPlaying() {
   var url =
@@ -171,14 +172,81 @@ function nbaLatestNews(newsData) {
   }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add("is-active");
+  }
+
+  function closeModal($el) {
+    $el.classList.remove("is-active");
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener("click", () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
+
+    $close.addEventListener("click", () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      closeAllModals();
+    }
+  });
+});
+// create persistency when searching for NBA teams. 
+var saveSearchHistory = function (team) {
+  var saveTeams = localStorage.getItem("teams")
+    ? JSON.parse(localStorage.getItem("teams"))
+    : [];
+
+    searchHistory = saveTeams;
+    searchHistory.push(team);
+    localStorage.setItem("teams", JSON.stringify(searchHistory));
+};
+
+// load recently searched NBA teams. 
+var loadSearchHistory = function (){
+ var recentSearch =  localStorage.getItem("teams");
+ console.log(recentSearch);
+  
+};
+
+// Display Searched Team Statistics and Blogs.
 teamInput.addEventListener("keypress", (event) => {
   if (event.keyCode === 13) {
     // key code of the keybord key
     event.preventDefault();
+    // save search history to localStorage.
+    saveSearchHistory(teamInput.value);
     // your code to Run
     location.href = "./team-search.html?team=" + teamInput.value;
   }
 });
-
+loadSearchHistory();
 getNowPlaying();
 getNBABlogs();
